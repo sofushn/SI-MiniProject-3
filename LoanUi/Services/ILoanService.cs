1c223df964e6c933ccb8bca7f46d4591b63758f5
sofushn <sofushn@hotmail.dk>
2021-11-06 17:10:37 +0100

    Added website that can request new loans

---
+++ LoanUi/Services/ILoanService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LoanUi.Services
+{
+    interface ILoanService
+    {
+        void RequestNewLoan(Guid customerId);
+    }
+}
---
+++ LoanUi/Services/LoanService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace LoanUi.Services
+{
+    public class LoanService: ILoanService
+    {
+        public void RequestNewLoan(Guid customerId)
+        {
+            ProducerConfig config = new()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            using IProducer<string, string> producer = new ProducerBuilder<string, string>(config).Build();
+            
+            string topic = "Loan.Request";
+            Message<string, string> message = new()
+            {
+                Key = customerId.ToString(),
+                Value = "New loan is requested"
+            };
+            producer.Produce(topic, message);
+        }
+    }
+}
